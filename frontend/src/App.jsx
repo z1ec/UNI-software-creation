8bd3805f1822153c472b8fe56cb22a17f3145cbb
SAVVA_ <savva2006@bk.ru>
2026-02-18 15:23:55 +0300

    v1.0
    test of PostgreSQL encode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,5 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-
 function App() {
-  const [message, setMessage] = useState('Loading...')
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message ?? 'No message'))
-      .catch(() => setMessage('Failed to load'))
-  }, [])
-
-  return (
-    <>
-      <h1>API Message</h1>
-      <p>{message}</p>
-    </>
-  )
+  return null
 }
 
 export default App
