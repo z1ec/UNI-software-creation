6abfc5fd053a7d8959ddf727a1d89d0d295fcda8
z1ec <vav3538@gmail.com>
2026-03-05 01:46:02 +0300

    add config files for tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,9 @@
-import { useEffect, useState } from 'react'
-
-type HelloResponse = {
-  message: string
-}
+import Header from "./components/Header"
 
 function App() {
-  const [data, setData] = useState<HelloResponse | null>(null)
-  const [error, setError] = useState<string | null>(null)
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(async (response) => {
-        if (!response.ok) {
-          throw new Error(`API error: ${response.status}`)
-        }
-        return (await response.json()) as HelloResponse
-      })
-      .then((json) => setData(json))
-      .catch((err: Error) => setError(err.message))
-  }, [])
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-slate-100 p-6">
-      <section className="w-full max-w-xl rounded-2xl border border-slate-200 bg-white p-8 shadow-sm">
-        <h1 className="mb-4 text-3xl font-bold text-slate-900">Frontend in Docker</h1>
-        {error && <p className="text-red-600">Ошибка: {error}</p>}
-        {!error && !data && <p className="text-slate-600">Загрузка...</p>}
-        {data && <p className="text-slate-700">Ответ backend: {data.message}</p>}
-      </section>
-    </main>
+    <Header />
   )
 }
 
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="sticky bg-white h-20">
+            <div className="mx-auto flex ">Clothing store</div>
+        </header>
+    )
+}
+
+export default Header
